791ed7d42802e6b5ae9a5929f09ce220d6d1472c
Mika Mantynen <107867422+mikamantynen@users.noreply.github.com>
2023-05-25 12:19:28 +0200

    Fixed the install.packages()

--- plot1.R
+++ plot1.R
@@ -1,3 +1,5 @@
+install.packages("chron")
+library(chron)
 data=read.table("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?",)
 data$Date=as.Date(data$Date,"%d/%m/%Y")
 stuff=data[(data$Date=="2007-02-01"|data$Date=="2007-02-02"),]
